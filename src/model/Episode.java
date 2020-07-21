a4135446d985e97807862cfe118a588f83aa8484
Lucas Viana Vilela <oviana@outlook.com>
2020-07-21 20:07:06 -0300

    Episode, Podcast

--- src/model/Episode.java
+++ src/model/Episode.java
@@ -1,37 +1,96 @@
 package model;
 
+import java.awt.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import javax.sound.sampled.AudioInputStream;
 
 public class Episode extends Audio {
-    
+
+    // ATTRIBUTES ___________________________________________________________________________________________
+
     private String description;
-    
-    /*
-    public Episode(String name, int lenght, List<People> cast, AudioInputStream audio, String description) {
-        super(name, lenght, cast, audio);
-        this.description = description;
-    }
-    */
+    private final LocalDate DATE; // String format YYYY-MM-DD
+    private Image cover;
 
-    public Episode(String name, AudioInputStream audio) {
+
+    // METHODS ______________________________________________________________________________________________
+
+    // Basic Constructor (current date)
+    public Episode(String name, AudioInputStream audio, String description, Image cover) {
         super(name, audio);
+        this.description = description;
+        this.cover = cover;
+        this.DATE = LocalDate.now();
     }
 
-    public Episode(String name, AudioInputStream audio, ArrayList<Featuring> feat) {
+    // Single-feat Constructor (current date)
+    public Episode(String name, AudioInputStream audio, Featuring feat, String description, Image cover) {
         super(name, audio, feat);
+        this.description = description;
+        this.cover = cover;
+        this.DATE = LocalDate.now();
     }
 
-    public Episode(String name, AudioInputStream audio, Featuring feat) {
+    // Multiple-feat Constructor (current date)
+    public Episode(String name, AudioInputStream audio, ArrayList<Featuring> feat, String description, Image cover) {
         super(name, audio, feat);
+        this.description = description;
+        this.cover = cover;
+        this.DATE = LocalDate.now();
     }
 
+    // Basic Constructor (specified date)
+    public Episode(String name, AudioInputStream audio, String description, Image cover, String releaseDate) {
+        super(name, audio);
+        this.description = description;
+        this.cover = cover;
+        this.DATE = LocalDate.parse(releaseDate);
+    }
+
+    // Single-feat Constructor (specified date)
+    public Episode(String name, AudioInputStream audio, Featuring feat, String description, Image cover, String releaseDate) {
+        super(name, audio, feat);
+        this.description = description;
+        this.cover = cover;
+        this.DATE = LocalDate.parse(releaseDate);
+    }
+
+    // Multiple-feat Constructor (specified date)
+    public Episode(String name, AudioInputStream audio, ArrayList<Featuring> feat, String description, Image cover, String releaseDate) {
+        super(name, audio, feat);
+        this.description = description;
+        this.cover = cover;
+        this.DATE = LocalDate.parse(releaseDate);
+    }
+
+    // Getters
     public String getDescription() {
         return description;
     }
 
+    public Image getCover(){
+        return this.cover;
+    }
+
+    public LocalDate getDATE(){
+        return this.DATE;
+    }
+
+    // Setters
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void setCover(Image cover){
+        this.cover = cover;
+    }
+
+    // Other methods
+    public void showInfo(){
+        super.showInfo();
+        System.out.println("\tDESCRIPTION: " + this.getDescription());
+        System.out.println("\tDATE: " + this.getDATE().toString());
     }
     
 }
--- src/model/Podcast.java
+++ src/model/Podcast.java
@@ -1,65 +1,157 @@
 package model;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Podcast {
-    
+
+    // ATTRIBUTES ___________________________________________________________________________________________
+
     private String name;
-    private List<Episode> episodes;
-    private List<Podcaster> presenter;
+    private int numberOfEpisodes;
+    private ArrayList<Episode> episodes = new ArrayList<>();
+    private ArrayList<Podcaster> hosts = new ArrayList<>();
+    private Image logo;
+    // gênero
 
-    public Podcast(String name, List<Episode> episodes, List<Podcaster> presenter) {
+    // METHODS ______________________________________________________________________________________________
+
+    // Basic constructor
+    public Podcast(String name, ArrayList<Episode> episodes, ArrayList<Podcaster> hosts) {
         this.name = name;
-        
-        this.episodes = new ArrayList<>();
-        episodes.forEach((episode) -> {
-            this.episodes.add(episode);
-        });
-        
-        this.presenter = new ArrayList<>();
-        presenter.forEach((podcaster) -> {
-            this.presenter.add(podcaster);
-        });
+        this.setEpisodes(episodes);
+        this.setHosts(hosts);
     }
 
+    // Logo constructor
+    public Podcast(String name, Image logo, ArrayList<Episode> episodes, ArrayList<Podcaster> hosts) {
+        this.name = name;
+        this.logo = logo;
+        this.setEpisodes(episodes);
+        this.setHosts(hosts);
+    }
+
+    // Genre constructor
+
+    // Complete constructor
+
+    // Getters
     public String getName() {
-        return name;
+        return this.name;
     }
 
-    public List<Episode> getEpisodes() {
-        return episodes;
+    public ArrayList<Episode> getEpisodes() {
+        return this.episodes;
     }
 
-    public List<Podcaster> getPresenter() {
-        return presenter;
+    public ArrayList<Podcaster> getHosts() {
+        return this.hosts;
     }
 
+    public Image getLogo(){
+        return this.logo;
+    }
+
+    public int getNumberOfEpisodes(){
+        return this.numberOfEpisodes;
+    }
+
+    // Setters
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setEpisodes(List<Episode> episodes) {
-        while(this.episodes.size() > 0)
-            this.episodes.remove(0);
-        
-        episodes.forEach((episode) -> {
-            this.episodes.add(episode);
-        });
+    public void setEpisodes(ArrayList<Episode> episodes) {
+        this.episodes.clear();
+        this.episodes.addAll(episodes);
+        this.numberOfEpisodes = this.episodes.size();
     }
 
-    public void setPresenter(List<Podcaster> presenter) {
-        while(this.presenter.size() > 0)
-            this.presenter.remove(0);
-        
-        this.presenter = new ArrayList<>();
-        presenter.forEach((podcaster) -> {
-            this.presenter.add(podcaster);
-        });
+    public void setHosts(ArrayList<Podcaster> hosts) {
+        this.hosts.clear();
+        this.hosts.addAll(hosts);
     }
-    
-    public void addNewEpisode(Episode episode) {
-        this.episodes.add(episode);
+
+    public void setNumberOfEpisodes(){
+        this.numberOfEpisodes = this.episodes.size();
+    }
+
+    // Other methods
+    /** "add" methods
+     * they add every new item that wasn't
+     * already in the list
+     */
+
+    public void addEpisode(Episode newEpisode) {
+        if(!this.episodes.contains(newEpisode)) {
+            this.episodes.add(newEpisode);
+            this.setNumberOfEpisodes();
+        }
+    }
+
+    public void addEpisodes(ArrayList<Episode> newEpisodes){
+        for(Episode newEpisode : newEpisodes){
+            this.addEpisode(newEpisode);
+        }
+    }
+
+    public void removeEpisode(Episode episode){
+        if (this.episodes.contains(episode)){
+            this.episodes.remove(episode);
+            this.setNumberOfEpisodes();
+        }
+    }
+
+    public void removeEpisode(int index){
+        this.episodes.remove(index);
+        this.setNumberOfEpisodes();
+    }
+
+    public void removeEpisodes(ArrayList<Episode> episodes){
+        for(Episode episode : episodes){
+            this.removeEpisode(episode);
+        }
+    }
+
+    public void removeEpisodes(int[] indexes){
+        for (int index : indexes){
+            this.removeEpisode(index);
+        }
+    }
+
+    public void addHost(Podcaster newHost){
+        if(!this.hosts.contains(newHost)){
+            this.hosts.add(newHost);
+        }
+    }
+
+    public void addHosts(ArrayList<Podcaster> newHosts){
+        for(Podcaster newHost : newHosts){
+            this.addHost(newHost);
+        }
+    }
+
+    public void removeHost(Podcaster exHost){
+        if(this.hosts.contains(exHost)){
+            this.hosts.remove(exHost);
+        }
+    }
+
+    public void removeHost(int index){
+        this.hosts.remove(index);
+    }
+
+    public void removeHosts(ArrayList<Podcaster> exHosts){
+        for(Podcaster exHost : exHosts){
+            this.removeHost(exHost);
+        }
+    }
+
+    public void removeHosts(int[] indexes){
+        for(int index : indexes){
+            this.removeHost(index);
+        }
     }
     
 }
